c104f4198692a8255a47c6b37c231f6d88376176
LucasKnezevich <knezevich.lucas@gmail.com>
2022-02-01 22:58:01 -0800

    Created FizzBuzz

---
+++ src/index.js
@@ -0,0 +1,49 @@
+// const express = require('express')
+import express from 'express';  // CAN'T GET THIS TO WORK
+
+
+import models, { connectDb } from './models';
+import routes from './routes';
+
+const app = express();
+
+// Built-In Middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('/public'));
+app.set('view engine', 'ejs');
+
+// Custom Middleware
+
+// Routes
+
+app.use('/api/todo', routes.todo);
+app.use('/fizz', routes.fizz);
+
+
+
+
+app.get('/', function (req, res) {
+    res.send('Hello!!')
+})
+
+const port = process.env.PORT || 4444;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
+
+// Database Seeding
+
+const createTodos = async () => {
+    const todo1 = new todoModel ({
+        title: "Feed the cat with the fish"
+    });
+    const todo2 = new todoModel( {
+        title: 'Walk the fish',
+    });
+    await todo1.save();
+    await todo2.save();
+};
+//createTodos(); // uncomment only once. Otherwise you will end up with many of the same
---
+++ src/routes/todo.js
@@ -0,0 +1,57 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', function (req, res){
+  models.Todo.find()
+  .then (function(todos){
+      res.json(todos);
+  })
+  .catch(function(err){
+      res.send(err);
+  })
+})
+
+router.get('/:id', function (req, res){
+  models.Todo.findById( req.params.id)
+  .then (function(todos){
+      res.json(todos);
+  })
+  .catch(function(err){
+      res.send(err);
+  })
+})
+
+router.post('/', function( req, res) {
+  models.Todo.create({
+      title : req.body.title,
+  })
+  .then(function(newTodo){
+      res.status(201).json(newTodo);
+  })
+  .catch(function(err) {
+      res.send(err);
+  });
+});
+
+router.put('/:id', function (req, res){
+  models.Todo.findOneAndUpdate( {id: req.params.id}, req.body, {new:true})
+  .then (function(todos){
+      res.json(todos);
+  })
+  .catch(function(err){
+      res.send(err);
+  })
+});
+
+router.delete('/:id', function (req, res){
+  models.Todo.deleteOne( { _id: req.params.id }, )
+  .then (function() {
+      res.json( { 'We deleted it!!': "we deleted it"});
+  })
+  .catch(function(err){
+      res.send(err);
+  });
+});
+
+export default router;
