9b30a0eef80fd86034b31d19d77de766bc831b43
Ladybird91 <65924582+Ladybird91@users.noreply.github.com>
2022-02-07 20:26:48 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,106 +1,52 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose');
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
 
+import models, { connectDb } from './models';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
 
 // Built-In Middleware
 
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('/public'));
 app.set('view engine', 'ejs');
 
-// Database Server MongoDb Setup
+// ROUTES
 
-mongoose.connect('mongodb://localhost/lopeznelson',
-    { useNewUrlParser: true });
-
-
-const todoSchema = new mongoose.Schema({
-    title: String,
-    isChecked: { type: Boolean, default: false },
-    date: { type: Date, default: new Date() }
-});
-
-const todoModel = mongoose.model("Todo", todoSchema);
-
-// Custom Middleware
-
-// Routes
-
-app.get('/api/todos', function (req, res){
-    todoModel.find()
-    .then (function(todos){
-        res.json(todos);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-})
-
-
-
-app.get('/api/todos/:id', function (req, res){
-    todoModel.findById( req.params.id)
-    .then (function(todos){
-        res.json(todos);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-})
-app.post('/api/todos', function( req, res) {
-    todoModel.create({
-        title : req.body.title,
-    })
-    .then(function(newTodo){
-        res.status(201).json(newTodo);
-    })
-    .catch(function(err) {
-        res.send(err);
-    });
-});
-
-app.put('/api/todos/:id', function (req, res){
-    todoModel.findOneAndUpdate( {id: req.params.id}, req.body, {new:true})
-    .then (function(todos){
-        res.json(todos);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
-
-app.delete('/api/todos/:id', function (req, res){
-    todoModel.deleteOne( { _id: req.params.id }, )
-    .then (function() {
-        res.json( { 'We deleted it!!': "we deleted it"});
-    })
-    .catch(function(err){
-        res.send(err);
-    });
-});
-
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
-
-const port = process.env.PORT || 4444;
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+app.use('/', routes.root);
+app.use('/api/todos', routes.todo);
+app.use('/api/fizzbuzz', routes.fizzbuzz);
 
 // Database Seeding
 
 const createTodos = async () => {
-    const todo1 = new todoModel ({
+    const todo1 = new models.Todo ({
         title: "Feed the cat with the fish"
     });
-    const todo2 = new todoModel( {
+    const todo2 = new models.Todo ( {
         title: 'Walk the fish',
     });
     await todo1.save();
     await todo2.save();
 };
-//createTodos(); // uncomment only once. Otherwise you will end up with many of the same
+
+
+const eraseDatabaseOnSync = true;
+connectDb().then(async() =>{
+    if(eraseDatabaseOnSync){
+        await Promise.all([
+            models.Todo.deleteMany({}),
+        ]);
+
+        createTodos();
+    }
+
+    app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`),
+);
+});
