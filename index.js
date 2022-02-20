7b52a2a0049ee8fd1f42125c1e810baab31057f0
Nelson as studentalfa <nelson.lopezjimenez@seattlecolleges.edu>
2022-02-20 15:23:50 -0800

    Added full js object to post route, pending rest operator

--- index.js
+++ index.js
@@ -25,26 +25,26 @@ const todoSchema = new mongoose.Schema({
 const todoModel = mongoose.model("Todo", todoSchema);
 
 const customerSchema = new mongoose.Schema({
-    id: {type: String, },
-    name: {type: String, },
-    username: {type: String, },
-    email: {type: String, },
+    id: { type: String, },
+    name: { type: String, },
+    username: { type: String, },
+    email: { type: String, },
     address: {
-        street: {type: String, },
-        suite: {type: String, },
-        city: {type: String, },
-        zipcode: {type: String, },
+        street: { type: String, },
+        suite: { type: String, },
+        city: { type: String, },
+        zipcode: { type: String, },
         geo: {
-            lat: {type: String, },
-            lng: {type: String, },
+            lat: { type: String, },
+            lng: { type: String, },
         },
     },
-    phone: {type: String, },
-    website: {type: String, },
+    phone: { type: String, },
+    website: { type: String, },
     company: {
-        name: {type: String, },
-        catchPhrase: {type: String, },
-        bs: {type: String, },
+        name: { type: String, },
+        catchPhrase: { type: String, },
+        bs: { type: String, },
     },
 });
 
@@ -53,106 +53,126 @@ const Customer = mongoose.model('Customer', customerSchema);
 
 // Routes
 
-app.get('/api/customers', function (req, res){
+app.get('/api/customers', function (req, res) {
     Customer.find()
-    .then (function(customers){
-        res.json(customers);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
+        .then(function (customers) {
+            res.json(customers);
+        })
+        .catch(function (err) {
+            res.send(err);
+        })
 })
 
-app.get('/api/customers/:id', function (req, res){
-    Customer.findById( req.params.id)
-    .then (function(customers){
-        res.json(customers);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
+app.get('/api/customers/:id', function (req, res) {
+    Customer.findById(req.params.id)
+        .then(function (customers) {
+            res.json(customers);
+        })
+        .catch(function (err) {
+            res.send(err);
+        })
 })
-app.post('/api/customers', function( req, res) {
+app.post('/api/customers', function (req, res) {
     Customer.create({
-        title : req.body.title,
+        id: req.body.id,
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+        address: {
+            street: req.body.address.street,
+            suite: req.body.address.suite,
+            city: req.body.address.city,
+            zipcode: req.body.zipcode,
+            geo: {
+                lat: req.body.address.geo.lat,
+                lng: req.body.address.geo.lng,
+            },
+        },
+        phone: req.body.phone,
+        website: req.body.website,
+        company: {
+            name: req.body.company.name,
+            catchPhrase: req.body.company.catchPhrase,
+            bs: req.body.company.bs,
+        },
     })
-    .then(function(newTodo){
-        res.status(201).json(newTodo);
-    })
-    .catch(function(err) {
-        res.send(err);
-    });
+        .then(function (newTodo) {
+            res.status(201).json(newTodo);
+        })
+        .catch(function (err) {
+            res.send(err);
+        });
 });
 
-app.put('/api/customers/:id', function (req, res){
-    Customer.findOneAndUpdate( {id: req.params.id}, req.body, {new:true})
-    .then (function(customers){
-        res.json(customers);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
+app.put('/api/customers/:id', function (req, res) {
+    Customer.findOneAndUpdate({ id: req.params.id }, req.body, { new: true })
+        .then(function (customers) {
+            res.json(customers);
+        })
+        .catch(function (err) {
+            res.send(err);
+        })
 });
 
-app.delete('/api/customers/:id', function (req, res){
-    Customer.deleteOne( { _id: req.params.id }, )
-    .then (function() {
-        res.json( { 'We deleted it!!': "we deleted it"});
-    })
-    .catch(function(err){
-        res.send(err);
-    });
+app.delete('/api/customers/:id', function (req, res) {
+    Customer.deleteOne({ _id: req.params.id },)
+        .then(function () {
+            res.json({ 'We deleted it!!': "we deleted it" });
+        })
+        .catch(function (err) {
+            res.send(err);
+        });
 });
 
-app.get('/api/todos', function (req, res){
+app.get('/api/todos', function (req, res) {
     todoModel.find()
-    .then (function(todos){
-        res.json(todos);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
+        .then(function (todos) {
+            res.json(todos);
+        })
+        .catch(function (err) {
+            res.send(err);
+        })
 })
 
-app.get('/api/todos/:id', function (req, res){
-    todoModel.findById( req.params.id)
-    .then (function(todos){
-        res.json(todos);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
+app.get('/api/todos/:id', function (req, res) {
+    todoModel.findById(req.params.id)
+        .then(function (todos) {
+            res.json(todos);
+        })
+        .catch(function (err) {
+            res.send(err);
+        })
 })
-app.post('/api/todos', function( req, res) {
+app.post('/api/todos', function (req, res) {
     todoModel.create({
-        title : req.body.title,
+        title: req.body.title,
     })
-    .then(function(newTodo){
-        res.status(201).json(newTodo);
-    })
-    .catch(function(err) {
-        res.send(err);
-    });
+        .then(function (newTodo) {
+            res.status(201).json(newTodo);
+        })
+        .catch(function (err) {
+            res.send(err);
+        });
 });
 
-app.put('/api/todos/:id', function (req, res){
-    todoModel.findOneAndUpdate( {id: req.params.id}, req.body, {new:true})
-    .then (function(todos){
-        res.json(todos);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
+app.put('/api/todos/:id', function (req, res) {
+    todoModel.findOneAndUpdate({ id: req.params.id }, req.body, { new: true })
+        .then(function (todos) {
+            res.json(todos);
+        })
+        .catch(function (err) {
+            res.send(err);
+        })
 });
 
-app.delete('/api/todos/:id', function (req, res){
-    todoModel.deleteOne( { _id: req.params.id }, )
-    .then (function() {
-        res.json( { 'We deleted it!!': "we deleted it"});
-    })
-    .catch(function(err){
-        res.send(err);
-    });
+app.delete('/api/todos/:id', function (req, res) {
+    todoModel.deleteOne({ _id: req.params.id },)
+        .then(function () {
+            res.json({ 'We deleted it!!': "we deleted it" });
+        })
+        .catch(function (err) {
+            res.send(err);
+        });
 });
 
 app.get('/', function (req, res) {
@@ -168,10 +188,10 @@ app.listen(port, () => {
 // Database Seeding
 
 const createTodos = async () => {
-    const todo1 = new todoModel ({
+    const todo1 = new todoModel({
         title: "Feed the cat with the fish"
     });
-    const todo2 = new todoModel( {
+    const todo2 = new todoModel({
         title: 'Walk the fish',
     });
     await todo1.save();
